6dc3e0f7f92b9cf3a00b4f69574a4da04b361ecc
Stephen Scott <scottie1984@gmail.com>
2020-04-07 12:16:38 +0100

    Added mongo

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ## Requirements
 
 * Node 10.13.0
+* Docker
 
 ## Set up
+On a seperate terminal tab
+```
+npm run dependency
+```
 ```
 npm install
 npm start
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict'
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongo = require('./mongo')
 
 const app = express()
 app.use(bodyParser.json())
@@ -8,6 +9,18 @@ app.use(bodyParser.json())
 app.get('/ping', (req, res) => {
   res.send({
     message: 'Welcome to the internet!'
+  })
+})
+
+app.get('/restaurants', async (req, res) => {
+  const restaurants = await mongo.finderFromRestaurantsEmpty()
+  res.send(restaurants)
+})
+
+app.post('/restaurant/create', async (req, res) => {
+  await mongo.insertToRestaurants(req.body)()
+  res.send({
+    message: 'Restaurant inserted successfully'
   })
 })
 
