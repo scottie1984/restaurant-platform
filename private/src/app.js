8e17c8ae602a162c6ebfee27558294a44d1d692a
Stephen Scott <scottie1984@gmail.com>
2020-04-08 15:10:01 +0100

    Added signup email

--- private/src/app.js
+++ private/src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongo = require('./mongo')
 
-const whitelist = ['http://localhost:8000', 'http://localhost:3000']
+const whitelist = ['http://localhost:8000', 'http://localhost:3000', 'https://master.d2gtcpj8lj87zj.amplifyapp.com/']
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -17,27 +17,37 @@ const corsOptions = {
 }
 
 const adminCors = cors(corsOptions)
-const publicCors = cors()
 
 const app = express()
 app.use(bodyParser.json())
 
 app.options('*', cors())
 
-app.get('/ping', publicCors, (req, res) => {
+const getEmail = req => req.event.requestContext.authorizer.claims.email
+
+app.get('/ping', adminCors, (req, res) => {
   res.send({
     message: 'Welcome to the internet!'
   })
 })
 
-app.get('/restaurants', publicCors, async (req, res) => {
-  console.log('event.requestContext.authorizer.claims.email', req.event.requestContext.authorizer)
+app.get('/restaurants', adminCors, async (req, res) => {
+  const signupEmail = getEmail(req)
   const restaurants = await mongo.finderFromRestaurantsEmpty()
-  res.send(restaurants)
+  const response = {
+    signupEmail,
+    restaurants
+  }
+  res.send(response)
 })
 
 app.post('/restaurants/create', adminCors, async (req, res) => {
-  await mongo.insertToRestaurants(req.body)()
+  const signupEmail = getEmail(req)
+  const data = {
+    signupEmail,
+    ...req.body
+  }
+  await mongo.insertToRestaurants(data)()
   res.send({
     message: 'Restaurant inserted successfully'
   })
--- public/src/app.js
+++ public/src/app.js
@@ -4,19 +4,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongo = require('./mongo')
 
-const whitelist = ['http://localhost:8000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true
-}
-
-const adminCors = cors(corsOptions)
 const publicCors = cors()
 
 const app = express()
@@ -31,13 +18,6 @@ app.get('/ping', publicCors, (req, res) => {
 app.get('/restaurants', publicCors, async (req, res) => {
   const restaurants = await mongo.finderFromRestaurantsEmpty()
   res.send(restaurants)
-})
-
-app.post('/restaurants/create', adminCors, async (req, res) => {
-  await mongo.insertToRestaurants(req.body)()
-  res.send({
-    message: 'Restaurant inserted successfully'
-  })
 })
 
 module.exports = app
